cb101f5f6457b5bcd30eed628abbf3d69de0edd5
Crowdbotics <team@crowdbotics.com>
2021-11-20 09:11:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "uber_32054",
+  url: "https://uber_32054.botics.co",
+  api: "https://uber_32054.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
